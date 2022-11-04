6ee3a37e175c6a40ddd34921e4a6393f92acf12c
Course Test <course-test@cs.sfu.ca>
2022-11-04 00:20:27 +0000

    done almost done exp1

--- Exp1.cpp
+++ Exp1.cpp
@@ -3,9 +3,9 @@
 #include "BinarySearchTree.h"
 using namespace std;
 
-    double elapsed_time( clock_t start, clock_t finish){ // returns elapsed time in milliseconds 
-        return (finish - start)/(double)(CLOCKS_PER_SEC/1000); 
-    } 
+double elapsed_time( clock_t start, clock_t finish){ // returns elapsed time in milliseconds 
+    return (finish - start)/(double)(CLOCKS_PER_SEC/1000); 
+} 
 
 int main( ) {
     BinarySearchTree<int> t1; // Tall and Skinny
@@ -39,35 +39,64 @@ int main( ) {
     } 
 
     cout << "Start of Exp1" << endl;
+    clock_t start, finish;
 
     // Test on overall time of first tree
-    clock_t start, finish;
     start = clock();
 
+    /*
     cout << "Links:" << endl ;
     t1.displayLinks(); 
     cout << "Height: " << endl;
     t1.height();
     cout << "Depth of Node 8: " << endl;
-    t1.depth(8);
+    t1.depth(8);*/
+
+    for( int n = 0 ; n <= 19 ; n++ ){
+        //key = (key + GAP) % CAP ;
+        t1.depth( n );
+    } 
 
     finish = clock(); 
 
     double overall_time1 = elapsed_time(start, finish);
 
+    start = clock();
+    t1.depth(18);
+    finish = clock();
+
+    double single_time1 = elapsed_time(start, finish);
+
+
     // Test on overall time of second tree
     start = clock();
 
+    /*
     cout << "Links:" << endl ;
     t2.displayLinks(); 
     cout << "Height: " << endl;
     t2.height();
     cout << "Depth of Node 8: " << endl;
-    t2.depth(8);    
+    t2.depth(8);*/
+
+    for(int q = 0 ; q <= 19 ; q++ ){
+        t2.depth( q );
+    }      
 
     finish = clock();
 
     double overall_time2 = elapsed_time(start, finish); 
+
+    start = clock();
+    t2.depth(18);
+    finish = clock();
+
+    double single_time2 = elapsed_time(start, finish);
+
+
+
+
+
 
     // Results 
     cout << "Time taken for the first tree: " << endl;
@@ -76,4 +105,13 @@ int main( ) {
 
     cout << "Time taken for the second tree: " << endl;
     cout << overall_time2 << endl;
+    cout << endl;
+
+    cout << "Time taken for depth of single node in the first tree: " << endl;
+    cout << single_time1 << endl;
+    cout << endl;    
+
+    cout << "Time taken for depth of single node in the second tree: " << endl;
+    cout << single_time2 << endl;
+    cout << endl;
 }
